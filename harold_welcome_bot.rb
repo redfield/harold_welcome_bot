426fece5d34cbae7690cc0bd82453f65a8ce35b2
Pavel Urzhumov <up.redfield@gmail.com>
2021-09-29 20:15:16 +0200

    Added a check for message type

--- harold_welcome_bot.rb
+++ harold_welcome_bot.rb
@@ -6,14 +6,19 @@ $stdout.sync = true
 puts "[#{Time.now}] Bot started"
 Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
   bot.listen do |message|
-    unless message.new_chat_members.empty?
-      message.new_chat_members.each do |user|
-        bot.api.send_sticker(
-          chat_id: message.chat.id,
-          reply_to_message_id: message.message_id,
-          sticker: 'CAACAgIAAxkBAAMZYVSYYQbI48MxD2QaA94x2ROqgKsAAkIAA2iaXQye3L-VT87R6CEE'
-        )
+    case message
+    when Telegram::Bot::Types::Message
+
+      unless message.new_chat_members.empty?
+        message.new_chat_members.each do |user|
+          bot.api.send_sticker(
+            chat_id: message.chat.id,
+            reply_to_message_id: message.message_id,
+            sticker: 'CAACAgIAAxkBAAMZYVSYYQbI48MxD2QaA94x2ROqgKsAAkIAA2iaXQye3L-VT87R6CEE'
+          )
+        end
       end
+
     end
   end
 end
