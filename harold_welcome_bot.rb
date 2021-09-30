8a6ea98ae4ce90299035bee380f276e8c76e494e
Pavel Urzhumov <up.redfield@gmail.com>
2021-09-30 08:50:11 +0200

    Add: bot replies on mentions and replies

--- harold_welcome_bot.rb
+++ harold_welcome_bot.rb
@@ -3,11 +3,31 @@ require 'telegram/bot'
 $stdout.reopen("#{__dir__}/harold_welcome_bot.log", 'a')
 $stdout.sync = true
 
+reply_stikers = [
+  'CAACAgIAAx0CVbzQLgADFWFVUi5KR-bNL8qVa8a9jH7ejSX0AAIvAAN5cd4WBMuBddyJMbQhBA',
+  'CAACAgIAAx0CVbzQLgADFmFVVgi2boNHRBxpfMVs0HSMwUhCAAIjAAN5cd4W2rS42UaRM5EhBA',
+  'CAACAgIAAx0CVbzQLgADH2FVWYN_kK8LrtMKn5mJX3l2B20-AAIuAAN5cd4WSnS803pjo38hBA',
+  'CAACAgIAAx0CVbzQLgADGWFVWQABF2ch2Po8nGpLnSODFZI-DAACLAADeXHeFhzvnOoJMqh8IQQ',
+  'CAACAgIAAx0CVbzQLgADG2FVWSZKmnfk2FwjTIoChNZHenK_AAISAAN5cd4WveSooL1rRLUhBA',
+  'CAACAgIAAx0CVbzQLgADHWFVWVRxsGmTeEhoDZB-hCu15yIqAAIRAAN5cd4WlieYjp-T1kshBA',
+  'CAACAgIAAx0CVbzQLgADIWFVWaE6C4t9i6C6AtzNqMBRUwABeQACGAADeXHeFlkmTdPzTa_tIQQ',
+  'CAACAgIAAx0CVbzQLgADI2FVWbwuQcKVdb7WW5QaacL7Nsc4AAIzAAN5cd4Wifg5Yb2ZBPAhBA'
+]
+
 puts "[#{Time.now}] Bot started"
 Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
   bot.listen do |message|
     case message
     when Telegram::Bot::Types::Message
+
+      reply = message.reply_to_message
+      if (message.text || '')['@HaroldWelcomeBot'] || (reply != nil && reply.from.username === 'HaroldWelcomeBot')
+        bot.api.send_sticker(
+          chat_id: message.chat.id,
+          reply_to_message_id: message.message_id,
+          sticker: reply_stikers.sample
+        )
+      end
 
       unless message.new_chat_members.empty?
         message.new_chat_members.each do |user|
