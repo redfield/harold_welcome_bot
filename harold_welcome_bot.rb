07141dc36c5341f83aff86bf2f500889fe61d45f
Pavel Urzhumov <up.redfield@gmail.com>
2021-11-29 20:09:56 +0200

    Patch for telegram errors handling

--- harold_welcome_bot.rb
+++ harold_welcome_bot.rb
@@ -1,5 +1,21 @@
 require 'telegram/bot'
 
+module Telegram
+  module Bot
+    class Api
+      def call(endpoint, raw_params = {})
+        params = build_params(raw_params)
+        response = conn.post("/bot#{token}/#{endpoint}", params)
+        if response.status == 200
+          JSON.parse(response.body)
+        else
+          puts "[#{Time.now}] API error: #{response.status} #{response.body}"
+        end
+      end
+    end
+  end
+end
+
 $stdout.reopen("#{__dir__}/harold_welcome_bot.log", 'a')
 $stdout.sync = true
 
